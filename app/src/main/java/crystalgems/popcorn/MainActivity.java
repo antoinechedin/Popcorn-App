d27a90ebb5c2ef2b1c819d87a7640036d75975b4
Alex Buleon <alex.buleon@gmail.com>
2017-03-25 19:30:13 +0100

    Begining of the implementation of home page
    Add xml
    Add fragment
    Add menu

--- app/src/main/java/crystalgems/popcorn/MainActivity.java
+++ app/src/main/java/crystalgems/popcorn/MainActivity.java
@@ -1,13 +1,24 @@
 package crystalgems.popcorn;
 
+import android.support.design.widget.TabLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+
+import static android.support.design.R.styleable.TabLayout;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.sing_in_sing_up_activity);
+        setContentView(R.layout.home_activity);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.home_menu, menu);
+        return true;
+    }
+
 }
