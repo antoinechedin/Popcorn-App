495282bef4833973f46a663f08629467a6a6c873
Alex Buleon <alex.buleon@gmail.com>
2017-03-25 21:46:20 +0100

    WiP : recycler view to display grid list (home page)

--- app/src/main/java/crystalgems/popcorn/MainActivity.java
+++ app/src/main/java/crystalgems/popcorn/MainActivity.java
@@ -4,23 +4,42 @@ import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.Menu;
 
 public class MainActivity extends AppCompatActivity {
+    private ViewPager viewPager;
+    private TabLayout tabLayout;
+    private RecyclerView homeRecyclerView;
+    private RecyclerView.LayoutManager homeLayoutManager;
+    private RecyclerView.Adapter rvAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home_activity);
 
-        // Get the ViewPager and set it's PagerAdapter so that it can display items
-        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
+        // Get the ViewPager and set its PagerAdapter so that it can display items
+        viewPager = (ViewPager) findViewById(R.id.viewpager);
         viewPager.setAdapter(new HomeFragmentPagerAdapter(getSupportFragmentManager(),
                 MainActivity.this));
 
         // Give the TabLayout the ViewPager
-        TabLayout tabLayout = (TabLayout) findViewById(R.id.sliding_tabs);
+        tabLayout = (TabLayout) findViewById(R.id.sliding_tabs);
         tabLayout.setupWithViewPager(viewPager);
+
+        homeRecyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
+
+        // Improve performance if we know components will have fixed size, which is the case
+        homeRecyclerView.setHasFixedSize(true);
+
+        homeLayoutManager = new GridLayoutManager(this, 2);
+        homeRecyclerView.setLayoutManager(homeLayoutManager);
+
+        // specify an adapter (see also next example)
+        rvAdapter = new CustomAdapter(customDataset); //TODO
+        homeRecyclerView.setAdapter(rvAdapter);
     }
 
     @Override
