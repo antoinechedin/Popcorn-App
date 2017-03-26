7b42dd8df49c6f9050615f8539d7c3fd60b31c46
Alex Buleon <alex.buleon@gmail.com>
2017-03-26 17:06:57 +0200

    Optimize imports

--- app/src/main/java/crystalgems/popcorn/MainActivity.java
+++ app/src/main/java/crystalgems/popcorn/MainActivity.java
@@ -1,14 +1,10 @@
 package crystalgems.popcorn;
 
-import android.content.Intent;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.RecyclerView;
 import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
     private ViewPager viewPager;
