8d4d0d4f47ee27cb439caed1cd9efaa9399b5b4e
Alex Buleon <alex.buleon@gmail.com>
2017-03-27 17:10:58 +0200

    Add horizontal recycler view in movie details
    Change grid layout to Staggered grid layout (3 lines max for the title)
    Add adapter for the new recycler view

--- app/src/main/java/crystalgems/popcorn/MovieDetailsActivity.java
+++ app/src/main/java/crystalgems/popcorn/MovieDetailsActivity.java
@@ -1,7 +1,12 @@
 package crystalgems.popcorn;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.StaggeredGridLayoutManager;
+import android.util.AttributeSet;
 import android.view.View;
 
 /**
@@ -9,11 +14,27 @@ import android.view.View;
  */
 
 public class MovieDetailsActivity extends Activity implements View.OnClickListener {
+    private RecyclerView movieDetailsRecyclerView;
+    private RecyclerView.LayoutManager movieDetailsLayoutManager;
+    private RecyclerView.Adapter rvAdapter;
+
+    private String[] customDataset = {"Premier", "Deuxième la la la la la la la la la la la la la la la la la la la la la la la la", "Troisième", "Quatrième la la la la la la la la la", "Cinquième", "Sixième", "Septième", "Huitième"};
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.movie_details);
+        movieDetailsRecyclerView = (RecyclerView) findViewById(R.id.movie_details_recycler_view);
+
+        // Improve performance if we know components will have fixed size, which is the case
+        movieDetailsRecyclerView.setHasFixedSize(true);
+
+        movieDetailsLayoutManager = new LinearLayoutManager(this, 0, false);
+        movieDetailsRecyclerView.setLayoutManager(movieDetailsLayoutManager);
+
+        // specify an adapter to create views for items in the recycler view
+        rvAdapter = new MovieDetailsRecyclerViewAdapter(customDataset);
+        movieDetailsRecyclerView.setAdapter(rvAdapter);
     }
 
     @Override
---
+++ app/src/main/java/crystalgems/popcorn/MovieDetailsRecyclerViewAdapter.java
@@ -0,0 +1,75 @@
+package crystalgems.popcorn;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+/**
+ * Created by Alex on 27/03/2017.
+ */
+
+public class MovieDetailsRecyclerViewAdapter extends RecyclerView.Adapter<MovieDetailsRecyclerViewAdapter.ViewHolder> {
+    private String[] dataset;
+    private Context context;
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        public View view;
+
+        public ViewHolder(final View vhView) {
+            super(vhView);
+            view = vhView;
+        }
+
+
+    }
+
+    public MovieDetailsRecyclerViewAdapter(String[] dataset) {
+        this.dataset = dataset;
+    }
+
+    // Create new views (invoked by the layout manager)
+    @Override
+    public MovieDetailsRecyclerViewAdapter.ViewHolder onCreateViewHolder(ViewGroup parent,
+                                                                 int viewType) {
+        this.context = parent.getContext();
+
+        // create a new view
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.movie_card_view, parent, false);
+        // We always can set the view's size, margins, paddings and layout parameters here
+
+        view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(context, MovieDetailsActivity.class);
+                context.startActivity(intent);
+            }
+        });
+
+        MovieDetailsRecyclerViewAdapter.ViewHolder viewHolder = new MovieDetailsRecyclerViewAdapter.ViewHolder(view);
+
+        return viewHolder;
+    }
+
+    // Replace the contents of a view (invoked by the layout manager)
+    @Override
+    public void onBindViewHolder(MovieDetailsRecyclerViewAdapter.ViewHolder holder, int position) {
+        // Movie picture
+        ImageView moviePicture = (ImageView) holder.view.findViewById(R.id.movie_picture);
+        moviePicture.setImageResource(R.drawable.la_la_land);
+
+        // Movie title
+        TextView movieTitle = (TextView) holder.view.findViewById(R.id.movieTitle);
+        movieTitle.setText(dataset[position]);
+    }
+
+    // Return the size of the dataset (invoked by the layout manager)
+    @Override
+    public int getItemCount() {
+        return dataset.length;
+    }
+}
