79557ca17862c40a83908105380e208dc88cfa6f
VitaliTInfo <johanna.chapman@etu.univ-tours.fr>
2017-03-27 22:06:00 +0200

    Change MovieDetailsActivity + Internet permission

--- app/src/main/java/crystalgems/popcorn/MovieDetailsActivity.java
+++ app/src/main/java/crystalgems/popcorn/MovieDetailsActivity.java
@@ -1,6 +1,7 @@
 package crystalgems.popcorn;
 
 import android.app.Activity;
+import android.app.DownloadManager;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -8,16 +9,29 @@ import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
+
+import java.io.BufferedInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 /**
  * Created by Alex on 26/03/2017.
  */
 
-public class MovieDetailsActivity extends Activity implements View.OnClickListener {
+public class MovieDetailsActivity extends Activity {
     private RecyclerView movieDetailsRecyclerView;
     private RecyclerView.LayoutManager movieDetailsLayoutManager;
     private RecyclerView.Adapter rvAdapter;
-    private Button rateButton;
+    private TextView releaseDateValue;
+    private TextView directorValue;
+    private TextView actorsValue;
+    private TextView categoriesValue;
+    private TextView nationalityValue;
+
 
     private String[] customDataset = {"Premier", "Deuxième la la la la la la la la la la la la la la la la la la la la la la la la", "Troisième", "Quatrième la la la la la la la la la", "Cinquième", "Sixième", "Septième", "Huitième"};
 
@@ -26,8 +40,15 @@ public class MovieDetailsActivity extends Activity implements View.OnClickListen
         super.onCreate(savedInstanceState);
         setContentView(R.layout.movie_details);
 
-        rateButton = (Button) findViewById(R.id.rateButton);
-        rateButton.setOnClickListener(this);
+        releaseDateValue = (TextView) findViewById(R.id.releaseDateValue);
+
+        directorValue = (TextView) findViewById(R.id.directorValue);
+
+        actorsValue = (TextView) findViewById(R.id.actorsValue);
+
+        categoriesValue = (TextView) findViewById(R.id.categoriesValue);
+
+        nationalityValue = (TextView) findViewById(R.id.nationalityValue);
 
         movieDetailsRecyclerView = (RecyclerView) findViewById(R.id.movie_details_recycler_view);
 
@@ -40,14 +61,8 @@ public class MovieDetailsActivity extends Activity implements View.OnClickListen
         // specify an adapter to create views for items in the recycler view
         rvAdapter = new MovieDetailsRecyclerViewAdapter(customDataset);
         movieDetailsRecyclerView.setAdapter(rvAdapter);
-    }
 
-    @Override
-    public void onClick(View v) {
-        if (v == rateButton) {
-            Intent intent = new Intent(this, RateActivity.class);
-            //TODO intent movie id to rate the right movie (putextra)
-            startActivity(intent);
-        }
+        
+
     }
 }
