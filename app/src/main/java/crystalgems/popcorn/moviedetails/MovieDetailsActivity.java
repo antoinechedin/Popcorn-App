db04bb8efe1def3b97d72de95b393aa8aeedb2dc
Alex Buleon <alex.buleon@gmail.com>
2017-03-28 17:12:46 +0200

    Add see more activity

--- app/src/main/java/crystalgems/popcorn/moviedetails/MovieDetailsActivity.java
+++ app/src/main/java/crystalgems/popcorn/moviedetails/MovieDetailsActivity.java
@@ -1,28 +1,37 @@
 package crystalgems.popcorn.moviedetails;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.widget.AppCompatButton;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
 import android.widget.TextView;
 
 import crystalgems.popcorn.R;
+import crystalgems.popcorn.seemore.SeeMoreRecommendationsActivity;
 
 /**
  * Created by Alex on 26/03/2017.
  */
 
-public class MovieDetailsActivity extends Activity {
+public class MovieDetailsActivity extends Activity implements View.OnClickListener {
     private RecyclerView movieDetailsGeneralRecommendationsRecyclerView;
     private RecyclerView movieDetailsActorsRecommendationsRecyclerView;
     private RecyclerView movieDetailsDirectorsRecommendationsRecyclerView;
     private RecyclerView movieDetailsGenresRecommendationsRecyclerView;
     private RecyclerView.Adapter rvAdapter;
+
     private TextView releaseDateValue;
     private TextView directorValue;
     private TextView actorsValue;
     private TextView categoriesValue;
     private TextView nationalityValue;
+    private AppCompatButton generalRecommendationsSeeMoreButton;
+    private AppCompatButton actorsRecommendationsSeeMoreButton;
+    private AppCompatButton directorsRecommendationsSeeMoreButton;
+    private AppCompatButton genresRecommendationsSeeMoreButton;
 
 
     private String[] customDataset = {"Premier", "Deuxième la la la la la la la la la la la la la la la la la la la la la la la la", "Troisième", "Quatrième la la la la la la la la la", "Cinquième", "Sixième", "Septième", "Huitième"};
@@ -37,6 +46,10 @@ public class MovieDetailsActivity extends Activity {
         actorsValue = (TextView) findViewById(R.id.actorsValue);
         categoriesValue = (TextView) findViewById(R.id.categoriesValue);
         nationalityValue = (TextView) findViewById(R.id.nationalityValue);
+        generalRecommendationsSeeMoreButton = (AppCompatButton) findViewById(R.id.generalRecommendationsSeeMoreButton);
+        actorsRecommendationsSeeMoreButton = (AppCompatButton) findViewById(R.id.actorsRecommendationsSeeMoreButton);
+        directorsRecommendationsSeeMoreButton = (AppCompatButton) findViewById(R.id.directorsRecommendationsSeeMoreButton);
+        genresRecommendationsSeeMoreButton = (AppCompatButton) findViewById(R.id.genresRecommendationsSeeMoreButton);
 
         movieDetailsGeneralRecommendationsRecyclerView = (RecyclerView) findViewById(R.id.movie_details_general_recommendations_recycler_view);
         movieDetailsActorsRecommendationsRecyclerView = (RecyclerView) findViewById(R.id.movie_details_actors_recommendations_recycler_view);
@@ -57,11 +70,33 @@ public class MovieDetailsActivity extends Activity {
         // specify an adapter to create views for items in the recycler view
         rvAdapter = new MovieDetailsRecyclerViewAdapter(customDataset);
         movieDetailsGeneralRecommendationsRecyclerView.setAdapter(rvAdapter);
-        movieDetailsActorsRecommendationsRecyclerView.setAdapter(rvAdapter); //TODO different adapter to match genre recommendations
-        movieDetailsDirectorsRecommendationsRecyclerView.setAdapter(rvAdapter); //TODO different adapter to match genre recommendations
-        movieDetailsGenresRecommendationsRecyclerView.setAdapter(rvAdapter); //TODO different adapter to match genre recommendations
+        movieDetailsActorsRecommendationsRecyclerView.setAdapter(rvAdapter); //TODO different adapter to match actors recommendations
+        movieDetailsDirectorsRecommendationsRecyclerView.setAdapter(rvAdapter); //TODO different adapter to match directors recommendations
+        movieDetailsGenresRecommendationsRecyclerView.setAdapter(rvAdapter); //TODO different adapter to match genres recommendations
 
+    }
 
-
+    @Override
+    public void onClick(View v) {
+        if (v == generalRecommendationsSeeMoreButton) {
+            Intent intent = new Intent(this, SeeMoreRecommendationsActivity.class);
+            //TODO intent put extra or set flags
+            startActivity(intent);
+        }
+        else if (v == actorsRecommendationsSeeMoreButton) {
+            Intent intent = new Intent(this, SeeMoreRecommendationsActivity.class);
+            //TODO intent put extra or set flags
+            startActivity(intent);
+        }
+        else if (v == directorsRecommendationsSeeMoreButton) {
+            Intent intent = new Intent(this, SeeMoreRecommendationsActivity.class);
+            //TODO intent put extra or set flags
+            startActivity(intent);
+        }
+        else if (v == genresRecommendationsSeeMoreButton) {
+            Intent intent = new Intent(this, SeeMoreRecommendationsActivity.class);
+            //TODO intent put extra or set flags
+            startActivity(intent);
+        }
     }
 }
---
+++ app/src/main/java/crystalgems/popcorn/seemore/SeeMoreRecommendationsActivity.java
@@ -0,0 +1,18 @@
+package crystalgems.popcorn.seemore;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import crystalgems.popcorn.R;
+
+/**
+ * Created by Alex on 28/03/2017.
+ */
+
+public class SeeMoreRecommendationsActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.see_more_recommendations);
+    }
+}
