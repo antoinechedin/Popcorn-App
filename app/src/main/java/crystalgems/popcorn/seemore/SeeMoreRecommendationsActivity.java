b86c1ba498c90ef40c13dedcdc1962c89dedff02
Alex Buleon <alex.buleon@gmail.com>
2017-03-29 20:52:02 +0200

    Add json async task and adapter
    Only the first title from the json query is read for now (one title)

--- app/src/main/java/crystalgems/popcorn/seemore/SeeMoreRecommendationsActivity.java
+++ app/src/main/java/crystalgems/popcorn/seemore/SeeMoreRecommendationsActivity.java
@@ -41,7 +41,7 @@ public class SeeMoreRecommendationsActivity extends Activity {
         homeRecyclerView.setLayoutManager(manager);
 
         // specify an adapter to create views for items in the recycler view
-        rvAdapter = new HomeRecyclerViewAdapter(customDataset);
+        rvAdapter = new HomeRecyclerViewAdapter();
         homeRecyclerView.setAdapter(rvAdapter);
 
     }
