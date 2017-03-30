a2d68cf9e14a9351c80f4fd32045131ae4c6b355
Alex Buleon <alex.buleon@gmail.com>
2017-03-30 12:34:52 +0200

    Movie poster is now display from an URL (always the same for now)
    Add a new asynctask to download images from url
    Add support for multiple json sources in the adapter
    Change movie card view layout to be always the right size

--- app/src/main/java/crystalgems/popcorn/QueriesManagement/JSONAsyncTask.java
+++ app/src/main/java/crystalgems/popcorn/QueriesManagement/JSONAsyncTask.java
@@ -18,47 +18,54 @@ import okhttp3.Response;
  * Created by Alex on 29/03/2017.
  */
 
-public class JSONAsyncTask extends AsyncTask<String, Void, String> {
+public class JSONAsyncTask extends AsyncTask<String, Void, String[]> {
 
     public interface StringConsumer {
         void setJSONString(String jsonString) throws JSONException;
     }
 
-    private OkHttpClient client;
-    private static Response response;
-    private StringConsumer jsonBody;
+    private OkHttpClient clientPopcorn;
+    private OkHttpClient clientImdb;
+    private static Response responsePopcorn;
+    private static Response responseImdb;
+    private StringConsumer jsonBodyText;
+    private StringConsumer jsonBodyPoster;
+    private String urlPopcornResponse;
+    private String urlImdbResponse;
 
     public JSONAsyncTask(StringConsumer stringConsumer) {
-        this.client = new OkHttpClient();
-        jsonBody = stringConsumer;
+        this.clientPopcorn = new OkHttpClient();
+        this.clientImdb = new OkHttpClient();
+        jsonBodyText = stringConsumer;
+        jsonBodyPoster = stringConsumer;
     }
 
     @Override
-    protected String doInBackground(String... params) {
+    protected String[] doInBackground(String... params) {
         try {
             URL urlPopcorn = new URL(params[0]);
             URL urlImdb = new URL(params[1]);
-            Request request = new Request.Builder().url(urlPopcorn).build();
-            response = client.newCall(request).execute();
+            Request requestPopcorn = new Request.Builder().url(urlPopcorn).build();
+            responsePopcorn = clientPopcorn.newCall(requestPopcorn).execute();
+            urlPopcornResponse = responsePopcorn.body().string();
+
+            Request requestImdb = new Request.Builder().url(urlImdb).build();
+            responseImdb = clientImdb.newCall(requestImdb).execute();
+            urlImdbResponse = responseImdb.body().string();
         }
         catch (IOException e) {
             e.printStackTrace();
         }
 
-        try {
-            return response.body().string();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        return "";
+        return new String[]{urlPopcornResponse, urlImdbResponse};
     }
 
     @Override
-    protected void onPostExecute(String result) {
+    protected void onPostExecute(String[] result) {
         Log.e("JSONAsyncTask", "Finished");
         try {
-            jsonBody.setJSONString(result);
+            jsonBodyText.setJSONString(result[0]);
+            jsonBodyPoster.setJSONString(result[1]);
         } catch (JSONException e) {
             e.printStackTrace();
         }
--- app/src/main/java/crystalgems/popcorn/homepage/HomeRecyclerViewAdapter.java
+++ app/src/main/java/crystalgems/popcorn/homepage/HomeRecyclerViewAdapter.java
@@ -2,7 +2,11 @@ package crystalgems.popcorn.homepage;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,6 +16,8 @@ import android.widget.TextView;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.io.InputStream;
 
 import crystalgems.popcorn.QueriesManagement.JSONAsyncTask;
 import crystalgems.popcorn.moviedetails.MovieDetailsActivity;
@@ -23,8 +29,10 @@ import crystalgems.popcorn.R;
 
 public class HomeRecyclerViewAdapter extends RecyclerView.Adapter<HomeRecyclerViewAdapter.ViewHolder> implements JSONAsyncTask.StringConsumer{
     private String[] dataset;
-    private JSONArray jsonArray;
-    private JSONObject jsonObject;
+    private JSONArray jsonParentArray = new JSONArray();
+    private JSONArray jsonChildArray = new JSONArray();
+    private JSONObject jsonPopcornObject;
+    private JSONObject jsonImdbObject;
     private Context context;
 
     public HomeRecyclerViewAdapter(String[] dataset) {
@@ -59,14 +67,55 @@ public class HomeRecyclerViewAdapter extends RecyclerView.Adapter<HomeRecyclerVi
     // Replace the contents of a view (invoked by the layout manager)
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
-        //Movie title and year
         try {
-            jsonObject = (JSONObject) jsonArray.get(position);
-            String movieTitle = jsonObject.getString("titleImdb");
-            String movieYear = jsonObject.getString("year");
+            jsonChildArray = (JSONArray) jsonParentArray.get(0);
+            jsonPopcornObject = (JSONObject) jsonChildArray.get(position);
+        }
+        catch (JSONException e) {
+            e.printStackTrace();
+        }
 
-            holder.setTitleYearElements(movieTitle, movieYear);
-        } catch (JSONException e) {
+        try {
+            if (jsonParentArray.length() > 1) {
+                jsonChildArray = (JSONArray) jsonParentArray.get(1);
+                jsonImdbObject = (JSONObject) jsonChildArray.get(0);
+            }
+        }
+        catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        //Movie title
+        try {
+            if (jsonImdbObject != null) {
+                String movieTitle = jsonPopcornObject.getString("titleImdb");
+                if (!movieTitle.equals(""))
+                    holder.setTitleElements(movieTitle);
+            }
+        }
+        catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        //Movie year
+        try {
+            if (jsonImdbObject != null) {
+                String movieYear = jsonPopcornObject.getString("year");
+                holder.setYearElements(movieYear);
+            }
+        }
+        catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        //Movie poster
+        try {
+            if (jsonImdbObject != null) {
+                String posterUrl = jsonImdbObject.getString("Poster");
+                holder.setPosterElements(posterUrl);
+            }
+        }
+        catch (JSONException e) {
             e.printStackTrace();
         }
     }
@@ -74,16 +123,35 @@ public class HomeRecyclerViewAdapter extends RecyclerView.Adapter<HomeRecyclerVi
     // Return the size of the dataset (invoked by the layout manager)
     @Override
     public int getItemCount() {
-        if (jsonArray != null)
-            return jsonArray.length();
+        if (jsonParentArray != null)
+            try {
+                JSONArray jsonArrayToReturn = (JSONArray) jsonParentArray.get(0);
+                return jsonArrayToReturn.length();
+            } catch (JSONException e) {
+                e.printStackTrace();
+                return 0;
+            }
         else
             return 0;
     }
 
     @Override
     public void setJSONString(String jsonString) throws JSONException {
-        jsonArray = new JSONArray(jsonString);
-        notifyDataSetChanged();
+        if (jsonString != null) {
+            if (!jsonString.equals("")) {
+                if (jsonString.substring(0,1).equals("[")) { //If the json is already an array
+                    jsonParentArray.put(new JSONArray(jsonString));
+                    System.out.println("JSON ARRAY IF:" + jsonParentArray);
+                }
+                else { //else, we get the array
+                    JSONObject searchObject = new JSONObject(jsonString);
+                    JSONArray arraySearchObject = (JSONArray) searchObject.get("Search");
+                    jsonParentArray.put(arraySearchObject);
+                    System.out.println("JSON ARRAY ELSE:" + jsonParentArray);
+                }
+                notifyDataSetChanged();
+            }
+        }
     }
 
     public static class ViewHolder extends RecyclerView.ViewHolder {
@@ -101,12 +169,43 @@ public class HomeRecyclerViewAdapter extends RecyclerView.Adapter<HomeRecyclerVi
         }
 
 
-        public void setTitleYearElements(String movieTitle, String moveYear) {
+        public void setTitleElements(String movieTitle) {
             movieTitleTextView.setText(movieTitle);
-            movieYearTextView.setText(moveYear);
+        }
 
+        public void setYearElements(String moveYear) {
+            movieYearTextView.setText(moveYear);
+        }
+
+        public void setPosterElements(String posterUrl) {
             //TODO salut
-            moviePosterImageView.setImageResource(R.drawable.la_la_land);
+            new DownloadImageTask(moviePosterImageView).execute(posterUrl);
+            //moviePosterImageView.setImageResource(R.color.cardview_dark_background);
+        }
+
+        private class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
+            ImageView bmImage;
+
+            public DownloadImageTask(ImageView bmImage) {
+                this.bmImage = bmImage;
+            }
+
+            protected Bitmap doInBackground(String... urls) {
+                String urldisplay = urls[0];
+                Bitmap mIcon11 = null;
+                try {
+                    InputStream in = new java.net.URL(urldisplay).openStream();
+                    mIcon11 = BitmapFactory.decodeStream(in);
+                } catch (Exception e) {
+                    Log.e("Error", e.getMessage());
+                    e.printStackTrace();
+                }
+                return mIcon11;
+            }
+
+            protected void onPostExecute(Bitmap result) {
+                bmImage.setImageBitmap(result);
+            }
         }
     }
 }
