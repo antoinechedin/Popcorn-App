84ba49f0d89021c2ae01b875839ee772a85e4c0a
Antoine Chédin <antoine.chedin@gmail.com>
2017-03-30 20:36:28 +0200

    Add user connexion

--- app/src/main/java/crystalgems/popcorn/SignInActivity.java
+++ app/src/main/java/crystalgems/popcorn/SignInActivity.java
@@ -3,19 +3,30 @@ package crystalgems.popcorn;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import crystalgems.popcorn.connexion.Connexion;
 import crystalgems.popcorn.homepage.HomeActivity;
+import crystalgems.popcorn.queriesManagement.AsyncTaskListener;
+import crystalgems.popcorn.queriesManagement.SimpleJSONAsyncTask;
 import crystalgems.popcorn.user.SignUpActivity;
 
 /**
  * Created by Tracy on 30/03/2017.
  */
 
-public class SignInActivity extends Activity {
+public class SignInActivity extends Activity implements AsyncTaskListener {
 
+    private AsyncTaskListener asyncTaskListener;
+    private EditText loginEditText;
+    private EditText passwordEditText;
     private Button homeButton;
     private Button signUpButton;
     private Context context;
@@ -24,16 +35,20 @@ public class SignInActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.sign_in_sign_up_activity);
+        asyncTaskListener = this;
 
         homeButton = (Button) findViewById(R.id.signInButton);
         signUpButton = (Button) findViewById(R.id.signUpButton);
+        loginEditText = (EditText) findViewById(R.id.loginEditText);
+        passwordEditText = (EditText) findViewById(R.id.passwordEditText);
+
         context = this;
 
         homeButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(context, HomeActivity.class);
-                startActivity(intent);
+                SimpleJSONAsyncTask asyncTask = new SimpleJSONAsyncTask(asyncTaskListener);
+                asyncTask.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR, "http://89.88.35.148:8080/popcorn/webapi/get/user-login?login=" + loginEditText.getText());
             }
         });
         signUpButton.setOnClickListener(new View.OnClickListener() {
@@ -43,7 +58,23 @@ public class SignInActivity extends Activity {
                 startActivity(intent);
             }
         });
+
+        if (Connexion.getInstance().getUserId() != 0) {
+            // TODO : Close this activity when start HomeActivity
+            Intent intent = new Intent(context, HomeActivity.class);
+            startActivity(intent);
+        }
     }
 
+    @Override
+    public void onPostAsyncTask(String jsonString) throws JSONException {
+        if (jsonString != null) {
+            JSONObject jsonObject = new JSONObject(jsonString);
+            int id = jsonObject.getInt("id");
+            Connexion.getInstance().setUserId(id);
 
+            Intent intent = new Intent(context, HomeActivity.class);
+            startActivity(intent);
+        }
+    }
 }
---
+++ app/src/main/java/crystalgems/popcorn/queriesManagement/SimpleJSONAsyncTask.java
@@ -0,0 +1,54 @@
+package crystalgems.popcorn.queriesManagement;
+
+import android.os.AsyncTask;
+import android.util.Log;
+
+import org.json.JSONException;
+
+import java.io.IOException;
+import java.net.URL;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+/**
+ * Created by Alex on 29/03/2017.
+ */
+
+public class SimpleJSONAsyncTask extends AsyncTask<String, Void, String> {
+    private static Response response;
+    private OkHttpClient client;
+    private AsyncTaskListener listener;
+    private String responseString;
+    private URL url;
+
+    public SimpleJSONAsyncTask(AsyncTaskListener asyncTaskListener) {
+        this.client = new OkHttpClient();
+        listener = asyncTaskListener;
+    }
+
+    @Override
+    protected String doInBackground(String... params) {
+        try {
+            url = new URL(params[0]);
+            Request requestPopcorn = new Request.Builder().url(url).build();
+            response = client.newCall(requestPopcorn).execute();
+            responseString = response.body().string();
+        }
+        catch (IOException e ) {
+        e.printStackTrace();
+    }
+        return responseString;
+    }
+
+    @Override
+    protected void onPostExecute(String resultString) {
+        Log.e("JSONAsyncTask " + url.toString(), "Finished");
+        try {
+            listener.onPostAsyncTask(resultString);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+}
