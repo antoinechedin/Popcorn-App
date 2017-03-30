27e52f6eb21ec09ea4782b74d328b67d18bac9ee
TrayCy <tracy.chpmn@yahoo.fr>
2017-03-30 19:15:22 +0200

    SignIn and SignUp Activities

--- app/src/main/java/crystalgems/popcorn/user/ProfileActivity.java
+++ app/src/main/java/crystalgems/popcorn/user/ProfileActivity.java
@@ -1,16 +1,10 @@
 package crystalgems.popcorn.user;
 
-import android.app.Activity;
 import android.os.Bundle;
-import android.support.design.widget.TabLayout;
-import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
-import android.view.View;
 
-import crystalgems.popcorn.MainActivity;
 import crystalgems.popcorn.R;
-import crystalgems.popcorn.homepage.HomeFragmentPagerAdapter;
 
 /**
  * Created by Tracy on 29/03/2017.
